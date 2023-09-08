45066cb1b7968c49b1e3ff9c1ef9236f88e38cf9
andolyrc <60825771+andolyrc@users.noreply.github.com>
2023-09-08 15:14:42 -0300

    database.php

--- bodegaHTML/database.php
+++ bodegaHTML/database.php
@@ -1,11 +1,11 @@
 <?php 
 
          
-     session_start();
+session_start();
 $DB_HOST=$_ENV["DB_HOST"];
 $DB_USER=$_ENV["DB_USER"];
-$DB_PASWOORD=$_ENV["DB_PASSWORD"];
+$DB_PASSWOORD=$_ENV["DB_PASSWORD"];
 $DB_NAME=$_ENV["DB_NAME"];
 $DB_PORT=$_ENV["DB_PORT"];
-$db=mysqli_connect("$DB_HOST","DB_USER","DB_PASWOORD","DB_NAME","DB_PORT")
+$db=mysqli_connect("DB_HOST","DB_USER","DB_PASSWOORD","DB_NAME","DB_PORT");
  ?>
