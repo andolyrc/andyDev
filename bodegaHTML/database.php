ee8e04cf1e907fbbf43589015ea7e8676058507d
andolyrc <60825771+andolyrc@users.noreply.github.com>
2023-09-08 16:21:36 -0300

    database.php

--- bodegaHTML/database.php
+++ bodegaHTML/database.php
@@ -1,11 +1,13 @@
 <?php 
 
          
+
+
 session_start();
-$DB_HOST=$_ENV["DB_HOST"];
-$DB_USER=$_ENV["DB_USER"];
-$DB_PASSWOORD=$_ENV["DB_PASSWORD"];
-$DB_NAME=$_ENV["DB_NAME"];
-$DB_PORT=$_ENV["DB_PORT"];
+$DB_HOST=$_ENV["containers-us-west-171.railway.app"];
+$DB_USER=$_ENV["root"];
+$DB_PASSWOORD=$_ENV["hkSDoJGSXbCXTJK5FYBN"];
+$DB_NAME=$_ENV["railway"];
+$DB_PORT=$_ENV["7172"];
 $db=mysqli_connect("DB_HOST","DB_USER","DB_PASSWOORD","DB_NAME","DB_PORT");
  ?>
